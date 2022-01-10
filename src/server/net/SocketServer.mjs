b686d49562640127df00fb560141962fc5fc7393
ooshhub <74662220+ooshhub@users.noreply.github.com>
2022-01-10 15:22:46 +1030

    Update server

--- src/server/net/SocketServer.mjs
+++ src/server/net/SocketServer.mjs
@@ -1,6 +1,7 @@
 // socketio server Class
 import { createServer } from 'http';
 import * as socketio from 'socket.io';
+import { helpers } from '../../shared/helpers.mjs';
 const { Server } = socketio;
 
 export class SocketServer {
@@ -32,7 +33,7 @@ export class SocketServer {
       path: options.path,
       connectTimeout: 5000,
     });
-
+		this.io.engine.on('connection_error', this.#handleGeneralConnectionError);
 		if (serverOptions.autoInitialize) this.initServer();
 	}
 	// Immediate middleware to upgrade http request ==> websocket
@@ -64,16 +65,55 @@ export class SocketServer {
 			this.#slog(`===UPGRADED CONNECTION FROM ${socket.handshake.address} ===`);
 			let playerDetails = socket.handshake.auth;
 			let rejection;
-			if (options.password && options.password !== playerDetails.password) err = new Error('Incorrect password!');
-			if (!playerDetails.playerName || !playerDetails.pid) err = new Error(`Bad player setup - missing PID or name`);
-			if (err) {
-				this.#slog(err, 'error');
+			if (this.config.password && this.config.password !== playerDetails.password) rejection = new Error('Incorrect password!');
+			if (!playerDetails.playerName || !playerDetails.pid) rejection = new Error(`Bad player setup - missing PID or name`);
+			if (rejection) {
+				this.#slog(rejection, 'error');
 				this.#addLogAttempt(cleanIp);
 				return;
 			}
-			let playerExists = this.#check
+			let playerExists = await this.#checkPlayerIsAlive(playerDetails);
+			if (playerExists !== undefined) {
+				this.#slog(`Player reconnect attempt: checking old socket...`);
+				if (playerExists) { // truthy return means player responded to ack
+					return this.#slog(`Player is already connected!`, 'warn');
+				} else { // null return means player exists on server but did not respond to ack
+					this.#slog(`Player ${playerDetails.playerName} - failed to respond to ack on existing socket`);
+					await this.#destroyPlayer(this.#playerList[playerDetails.pid], 'Failed to respond to ack request');
+				}
+			}
+			socket.emit('auth', playerDetails.pid);
+			playerDetails.isHost = this.#checkPlayerIsHost(playerDetails.pid);
+			playerDetails.socket = socket;
+			// Add player to server, init handlers
+			this.#playerList[playerDetails.pid] = playerDetails;
+			socket.on('disconnect', this.#handlePlayerDisconnect);
+			socket.on('message', (event, data, ...args) => this.#receiveFromClient(socket, event, data, ...args));
 		}
 	}
+	// Set up event hub link
+	#eventHub = [];
+	registerEventHub(eventHubLink) { if (/function/i.test(typeof(eventHubLink))) this.#eventHub.push(eventHubLink); }
+
+	async #receiveFromClient(socket, event, data, ...args) {
+		try { Object.assign(data, { sid: socket.id }) }
+		catch(e) { this.#slog(`Bad event received from client, data was not an Object`, 'warn') }
+		this.#triggerHub(event, data, ...args);
+	}
+	async #triggerHub(event, data, ...args) {
+		this.#eventHub.forEach(async (hub) => hub(event, data, ...args));
+	}
+	// TODO: Error handling
+	#handlePlayerDisconnect(reason) {
+		this.#slog([`Client disconnected:`, reason], 'warn');
+		// Fire a timer, if no reconnect, destroy player???
+	}
+	#handleGeneralConnectionError(details) {
+		this.#slog([`Connection error: `, details], 'warn');
+		// NFI what to do with this yet
+	}
+
+	#checkPlayerIsHost(playerId) { return (playerId === this.host.pid) ? true : false; }
 	// Supply playerData to check specific player, otherwise all players are checked
 	async #healthCheckAck(socket) {
 		return new Promise(res => socket.emit('healthCheck', (ack) => res(ack)));
@@ -87,6 +127,15 @@ export class SocketServer {
 			helpers.timeout(ackTimeout),
 			this.#healthCheckAck(this.#playerList[playerData.pid].socket)
 		]);
+	}
+	#destroyPlayer = async (pid, reasonForDestroy) => {
+		if (!this.#playerList[pid]) return this.#slog(`destroyPlayer: bad id "${pid}"`);
+		if (this.#playerList[pid].socket) {
+			this.#playerList[pid].socket.send('deathnote', { msg: reasonForDestroy }||`Just because you're a cunt.`);
+			this.#playerList[pid].socket.disconnect(true);
+		}
+		delete this.#playerList[pid];
+		/* TODO: Scrub player reference from this.#houseList */
 	}
 
 	// Direct server logging. TODO: change to subscription model
